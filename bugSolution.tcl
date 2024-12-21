7838eeb6ef265cb0266110754b04366087e47e07
BugHunter-X <askzenai@gmail.com>
2024-12-22 00:28:05 +0100

    Add bugSolution.tcl file

---
+++ bugSolution.tcl
@@ -0,0 +1,11 @@
+proc get_element {list index} { 
+  if {$index < 0 || $index >= [llength $list]} { 
+    return -code error "Index out of range" 
+  } 
+  return [lindex $list $index] 
+} 
+
+set mylist {a b c d e} 
+puts "Element at index 2: [get_element $mylist 2]"  ;#Correct output
+puts "Element at index 5: [catch {get_element $mylist 5} result]
+puts "Error: $result"
